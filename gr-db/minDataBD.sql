75d2d4e417cbf87f020e765a515b13a9e5b89ada
Guille <gmarcg00@estudiantes.unileon.es>
2023-06-04 01:41:30 +0200

    GR-25 reactions terminado

---
+++ gr-db/minDataBD.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `gr-db`.`user` (`email`, `password`, `user_name`) VALUES ('user1@gmail.com', 'user1-1234', 'user1');
+INSERT INTO `gr-db`.`user` (`email`, `password`, `user_name`) VALUES ('user2@hotmail.com', 'user2-1234', 'user2');
+INSERT INTO `gr-db`.`user` (`email`, `password`, `user_name`) VALUES ('user3@live.com', 'user3-1234', 'user3');
